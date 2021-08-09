bb4c7cd4f54bbf76526f8f6ef0afefd89d1e7d15
Dylan Martin <dmartin@cloudentity.com>
2021-08-09 16:46:29 -0400

    add endpoint logic interface; add generic server Get method

---
+++ apps/bank/handler.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type EndpointLogic interface {
+	SetIntrospectionResponse(c *gin.Context) error
+	MapError(err error) interface{}
+	BuildResponse(BankUserData) interface{}
+	Filter(data BankUserData) BankUserData
+	Validate() error
+	GetUserIdentifier(c *gin.Context) string
+}
+
+func (s *Server) Get(h EndpointLogic) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var (
+			data BankUserData
+			err  error
+		)
+
+		if err = h.SetIntrospectionResponse(c); err != nil {
+			c.PureJSON(http.StatusBadRequest, h.MapError(err))
+			return
+		}
+
+		if err = h.Validate(); err != nil {
+			c.PureJSON(http.StatusBadRequest, h.MapError(err))
+			return
+		}
+
+		if data, err = s.Storage.Get(h.GetUserIdentifier(c)); err != nil {
+			c.PureJSON(http.StatusBadRequest, h.MapError(err))
+			return
+		}
+
+		filtered := h.Filter(data)
+		c.PureJSON(http.StatusOK, h.BuildResponse(filtered))
+	}
+}
