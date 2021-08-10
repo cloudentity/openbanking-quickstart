135cd4d2b2b82287f9004295bc7ac2bea4762e62
Dylan Martin <dmartin@cloudentity.com>
2021-08-09 21:56:14 -0400

    add obuk utilities to separate file

---
+++ apps/bank/obuk_helpers.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"time"
+
+	acpClient "github.com/cloudentity/acp-client-go/models"
+	"github.com/cloudentity/openbanking-quickstart/openbanking/obuk/accountinformation/models"
+	paymentModels "github.com/cloudentity/openbanking-quickstart/openbanking/obuk/paymentinitiation/models"
+	"github.com/go-openapi/strfmt"
+)
+
+func OBUKMapError(err error) models.OBError1 {
+	msg := err.Error()
+	return models.OBError1{
+		Message: &msg,
+	}
+}
+
+func NewAccountsResponse(accounts []models.OBAccount6, self strfmt.URI) models.OBReadAccount6 {
+	accountsPointers := make([]*models.OBAccount6, len(accounts))
+
+	for i, a := range accounts {
+		account := a
+		accountsPointers[i] = &account
+	}
+
+	return models.OBReadAccount6{
+		Data: &models.OBReadAccount6Data{
+			Account: accountsPointers,
+		},
+		Meta: &models.Meta{
+			TotalPages: int32(len(accounts)),
+		},
+		Links: &models.Links{
+			Self: &self,
+		},
+	}
+}
+
+func NewBalancesResponse(balances []models.OBReadBalance1DataBalanceItems0, self strfmt.URI) models.OBReadBalance1 {
+	balancesPointers := make([]*models.OBReadBalance1DataBalanceItems0, len(balances))
+
+	for i, b := range balances {
+		balance := b
+		balancesPointers[i] = &balance
+	}
+
+	return models.OBReadBalance1{
+		Data: &models.OBReadBalance1Data{
+			Balance: balancesPointers,
+		},
+		Meta: &models.Meta{
+			TotalPages: int32(len(balances)),
+		},
+		Links: &models.Links{
+			Self: &self,
+		},
+	}
+}
+
+func has(list []string, a string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func newDateTimePtr(t time.Time) *strfmt.DateTime {
+	str := strfmt.DateTime(t)
+	return &str
+}
+
+func initiationsAreEqual(initiation1, initiation2 interface{}) bool {
+	var (
+		initiation1Bytes []byte
+		initiation2Bytes []byte
+		err              error
+	)
+	if initiation1Bytes, err = json.Marshal(initiation1); err != nil {
+		return false
+	}
+	if initiation2Bytes, err = json.Marshal(initiation2); err != nil {
+		return false
+	}
+	return bytes.Equal(initiation1Bytes, initiation2Bytes)
+}
+
+func toDomesticResponse5DataInitiation(initiation *acpClient.OBWriteDomesticConsentResponse5DataInitiation) *paymentModels.OBWriteDomesticResponse5DataInitiation {
+	var (
+		initiationBytes []byte
+		err             error
+		ret             paymentModels.OBWriteDomesticResponse5DataInitiation
+	)
+
+	if initiationBytes, err = json.Marshal(*initiation); err != nil {
+		panic(err)
+	}
+
+	if err = json.Unmarshal(initiationBytes, &ret); err != nil {
+		panic(err)
+	}
+
+	return &ret
+}
