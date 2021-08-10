5d20257bdb373d8755924a5689ef5db4ac170aaa
Dylan Martin <dmartin@cloudentity.com>
2021-08-09 23:20:10 -0400

    domestic payment implementation without validation

--- apps/bank/obuk_create_payment_handler.go
+++ apps/bank/obuk_create_payment_handler.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"strconv"
-	"time"
 
 	acpClient "github.com/cloudentity/acp-client-go/models"
 	paymentModels "github.com/cloudentity/openbanking-quickstart/openbanking/obuk/paymentinitiation/models"
@@ -37,90 +35,112 @@ func (h *OBUKCreatePaymentHandler) SetRequest(c *gin.Context) error {
 	return json.NewDecoder(c.Request.Body).Decode(&h.request)
 }
 
-func (h *OBUKCreatePaymentHandler) BuildResource(c *gin.Context) interface{} {
-	id := uuid.New().String()
-	status := string(AcceptedSettlementInProcess)
-	self := strfmt.URI(fmt.Sprintf("http://localhost:%s/domestic-payments/%s", strconv.Itoa(h.Config.Port), id))
-	response := paymentModels.OBWriteDomesticResponse5{
-		Data: &paymentModels.OBWriteDomesticResponse5Data{
-			DomesticPaymentID:    &id,
-			ConsentID:            &h.introspectionResponse.ConsentID,
-			Status:               &status,
-			Charges:              []*paymentModels.OBWriteDomesticResponse5DataChargesItems0{},
-			CreationDateTime:     newDateTimePtr(time.Now()),
-			StatusUpdateDateTime: newDateTimePtr(time.Now()),
-			Initiation:           toDomesticResponse5DataInitiation(h.introspectionResponse.Initiation),
-		},
-		Links: &paymentModels.Links{
-			Self: &self,
-		},
-	}
-
-	return response
-}
-
-func (h *OBUKCreatePaymentHandler) StoreResource(c *gin.Context, sub string, resource interface{}) (interface{}, error) {
+func (h *OBUKCreatePaymentHandler) CreateResource(c *gin.Context, sub string) (interface{}, error) {
 	var (
-		data BankUserData
-		res  paymentModels.OBWriteDomesticResponse5
-		err  error
-		ok   bool
+		data    BankUserData
+		id      = uuid.New().String()
+		self    = strfmt.URI(fmt.Sprintf("http://localhost:%s/domestic-payments/%s", strconv.Itoa(h.Config.Port), id))
+		payment = NewOBUKPayment(h.introspectionResponse, self, id)
+		err     error
 	)
-
-	if res, ok = resource.(paymentModels.OBWriteDomesticResponse5); !ok {
-		return "", errors.New("wrong resource model")
-	}
 
 	if data, err = h.Storage.Get(sub); err != nil {
 		return "", err
 	}
 
-	data.Payments = append(data.Payments, res)
+	// could just check if it already exists here...
+	data.Payments = append(data.Payments, payment)
 
-	if err = h.Storage.Put(sub, data); err != nil {
+	/*if err = h.Storage.Put(sub, data); err != nil {
 		return "", err
-	}
+	}*/
 
-	return res, nil
-}
-
-func (h *OBUKCreatePaymentHandler) ResourceAlreadyExists(c *gin.Context, sub string, resource interface{}) bool {
-	var (
-		data BankUserData
-		res  paymentModels.OBWriteDomesticResponse5
-		err  error
-		ok   bool
-	)
-
-	if res, ok = resource.(paymentModels.OBWriteDomesticResponse5); !ok {
-		return false
-	}
-
-	if data, err = h.Storage.Get(sub); err != nil {
-		return false
-	}
-
-	for _, payment := range data.Payments {
-		if payment.Data.ConsentID == res.Data.ConsentID {
-			return true
-		}
-	}
-
-	return false
+	return payment, nil
 }
 
 func (h *OBUKCreatePaymentHandler) SetIntrospectionResponse(c *gin.Context) error {
-	return nil
+	var err error
+	h.introspectionResponse, err = h.IntrospectPaymentsToken(c)
+	return err
 }
 
 func (h *OBUKCreatePaymentHandler) Validate(c *gin.Context) error {
+	/*scopes := strings.Split(introspectionResponse.Scope, " ")
+	if !has(scopes, "payments") {
+		msg := "token has no payments scope granted"
+		c.JSON(http.StatusForbidden, models.OBErrorResponse1{
+			Message: &msg,
+		})
+		return
+	}
+
+	if *introspectionResponse.Status != "Authorised" {
+		msg := "domestic payment consent does not have status authorised"
+		c.JSON(http.StatusUnprocessableEntity, models.OBError1{
+			Message: &msg,
+		})
+		return
+	}
+
+	if paymentRequest.Data.Initiation == nil {
+		msg := "initiation data not present in request"
+		c.JSON(http.StatusBadRequest, models.OBError1{
+			Message: &msg,
+		})
+		return
+	}
+
+	if introspectionResponse.Initiation == nil {
+		msg := "initiation data not present in introspection response"
+		c.JSON(http.StatusInternalServerError, models.OBError1{
+			Message: &msg,
+		})
+		return
+	}
+
+	if !initiationsAreEqual(*paymentRequest.Data.Initiation, *introspectionResponse.Initiation) {
+		msg := "request initiation does not match consent initiation"
+		c.JSON(http.StatusBadRequest, models.OBError1{
+			Message: &msg,
+		})
+		return
+	}
+
+	if paymentRequest.Risk == nil {
+		msg := "no risk data in payment request"
+		c.JSON(http.StatusBadRequest, models.OBError1{
+			Message: &msg,
+		})
+		return
+	}
+
+	paymentRisk := paymentRequest.Risk
+	consentRisk := &paymentModels.OBRisk1{}
+
+	if err = copier.Copy(consentRisk, introspectionResponse); err != nil {
+		msg := "internal error"
+		logrus.WithError(err).Error("field copying failed")
+		c.JSON(http.StatusInternalServerError, models.OBError1{
+			Message: &msg,
+		})
+		return
+	}
+
+	if !reflect.DeepEqual(paymentRisk, consentRisk) {
+		msg := "risk validation failed"
+		logrus.Errorf(msg)
+		c.JSON(http.StatusBadRequest, models.OBError1{
+			Message: &msg,
+		})
+		return
+	}*/
 	return nil
 }
 
 func (h *OBUKCreatePaymentHandler) MapError(c *gin.Context, err error) interface{} {
-	return nil
+	return OBUKMapError(err)
 }
 
 func (h *OBUKCreatePaymentHandler) GetUserIdentifier(c *gin.Context) string {
-	return ""
+	return h.introspectionResponse.Sub
 }
