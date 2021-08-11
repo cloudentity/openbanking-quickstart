379214198359ff00bba62305bfa6d04ee9314a09
Dylan Martin <dmartin@cloudentity.com>
2021-08-11 14:22:21 -0400

    obbr create payment request model

--- apps/bank/obbr_models.go
+++ apps/bank/obbr_models.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"github.com/cloudentity/openbanking-quickstart/openbanking/obbr/payments/models"
+)
+
+// swagger:parameters createOBBRPaymentRequest
+type CreateOBBRPaymentRequest struct {
+	RequestHeaders
+
+	// in:body
+	Request *models.OpenbankingBrasilCreatePixPayment
+}
