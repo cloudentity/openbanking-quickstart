e6bf1fe13325e3e459cbb7d2920b945c32a02ca2
Dylan Martin <dmartin@cloudentity.com>
2021-08-11 12:35:00 -0400

    obbr accounts response constructor

--- apps/bank/obbr_utils.go
+++ apps/bank/obbr_utils.go
@@ -1,7 +1,34 @@
 package main
 
-import "github.com/cloudentity/openbanking-quickstart/openbanking/obbr/consents/models"
+import (
+	"github.com/cloudentity/openbanking-quickstart/openbanking/obbr/consents/models"
+	"github.com/gin-gonic/gin"
+)
 
-func OBBRMapError(err error) interface{} {
-	return models.OpenbankingBrasilResponseError{}
+func OBBRMapError(c *gin.Context, err *Error) (int, interface{}) {
+	return err.Code, models.OpenbankingBrasilResponseError{
+		Errors: []*models.OpenbankingBrasilError{},
+	}
+}
+
+/*
+{
+  "errors": [
+    {
+      "code": "string",
+      "title": "string",
+      "detail": "string"
+    }
+  ],
+  "meta": {
+    "totalRecords": 1,
+    "totalPages": 1,
+    "requestDateTime": "2021-05-21T08:30:00Z"
+  }
+}
+*/
+func NewOBBRAccountsResponse(accounts []AccountData) ResponseAccountList {
+	return ResponseAccountList{
+		Data: accounts,
+	}
 }
