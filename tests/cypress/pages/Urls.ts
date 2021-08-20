2526c5c23cb1cb74feb69f353b0da8b847de5b62
Rajmund Nowicki <rnowicki@cloudentity.com>
2021-08-20 15:50:20 +0200

    AUT-3373 moved urls to env file.

--- tests/cypress/pages/Urls.ts
+++ tests/cypress/pages/Urls.ts
@@ -1,9 +1,4 @@
 export class Urls {
-  public static readonly tppTechnicalUrl: string = `https://localhost:8090`;
-  public static readonly financrooUrl: string = `https://localhost:8091`
-  public static readonly acpUrl: string = `https://localhost:8443`
-  public static readonly consentSelfServiceUrl: string = `https://localhost:8085`
-  public static readonly consentAdminUrl: string = `https://localhost:8086`
 
   public static visit(url: string, force: boolean) {
     if (force) {
