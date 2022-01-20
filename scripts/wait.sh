715621f3cafae7ff350883a51681d38f48f3c5db
Ireneusz Kawalec <ikawalec@cloudentity.com>
2022-01-20 13:54:51 +0100

    test

--- scripts/wait.sh
+++ scripts/wait.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TIMEOUT="${TIMEOUT:-30}"
+TIMEOUT="${TIMEOUT:-40}"
 
 echo "Waiting ${TIMEOUT} sec for ACP to be ready"
 timeout "${TIMEOUT}" bash -c 'while [[ "$(curl -s -k -o /dev/null -w "%{http_code}" https://localhost:8443/alive)" != 200 ]]; do sleep 1; done' || exit 1
