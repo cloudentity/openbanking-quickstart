445f2f8d7e070d2f3fe3545014a8334e8c40aa1b
Ireneusz Kawalec <ikawalec@cloudentity.com>
2022-01-27 10:53:58 +0100

    debug

--- scripts/wait.sh
+++ scripts/wait.sh
@@ -3,5 +3,5 @@
 TIMEOUT="${TIMEOUT:-90}"
 
 echo "Waiting ${TIMEOUT} sec for ACP to be ready"
-timeout "${TIMEOUT}" bash -c 'while [[ "$(curl -s -k -o /dev/null -w "%{http_code}" https://localhost:8443/alive)" != 200 ]]; do docker ps -a; sleep 1; done' || exit 1
+timeout "${TIMEOUT}" bash -c 'while [[ "$(curl -s -k -o /dev/null -w "%{http_code}" https://localhost:8443/alive)" != 200 ]]; do docker ps -a; free; docker stats --no-stream; sleep 1; done' || exit 1
 echo "ACP is ready"
