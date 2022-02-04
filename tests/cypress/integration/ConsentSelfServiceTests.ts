7a91ed652dd45359c473a3445375325311a08764
Dylan Martin <dmartin@cloudentity.com>
2022-02-04 13:16:39 -0500

    move cdr self service test to separate file, finish happy path test, and add revoke test

--- tests/cypress/integration/ConsentSelfServiceTests.ts
+++ tests/cypress/integration/ConsentSelfServiceTests.ts
@@ -14,46 +14,6 @@ import {ConsentSelfServiceApplicationPage} from "../pages/consent-self-service/C
 import {EnvironmentVariables} from "../pages/EnvironmentVariables"
 import { MockDataRecipientPage } from '../pages/mock-data-recipient/MockDataRecipientPage';
 
-describe(`Consent self service app CDR`, () => {
-  const acpLoginPage: AcpLoginPage = new AcpLoginPage();
-  const consentPage: ConsentPage = new ConsentPage();
-  const consentSelfServicePage: ConsentSelfServicePage = new ConsentSelfServicePage();
-  const consentSelfServiceApplicationPage: ConsentSelfServiceApplicationPage = new ConsentSelfServiceApplicationPage();
-  const environmentVariables: EnvironmentVariables = new EnvironmentVariables();
-  const mockDataRecipientPage: MockDataRecipientPage = new MockDataRecipientPage(); 
-
-  before(() => {
-    mockDataRecipientPage.visit()
-    
-    // TODO: refresh wait? 
-    mockDataRecipientPage.visitDiscoverDataHoldersTab()
-    mockDataRecipientPage.clickDataHoldersRefresh()
-
-    mockDataRecipientPage.visitDynamicClientRegistrationTab() 
-    mockDataRecipientPage.clickDCRRegisterButton()
-
-    mockDataRecipientPage.visitConsentAndAuthorisationTab() 
-    mockDataRecipientPage.selectClientRegistration(1)
-    mockDataRecipientPage.inputSharingDuration(1000000)
-    mockDataRecipientPage.clickConstructAuthorisationURI(); 
-    
-    acpLoginPage.login(Credentials.tppUsername, Credentials.defaultPassword);
-
-    consentPage.confirm()
-  })
-
-  beforeEach(() => {
-    consentSelfServicePage.visit(true)
-  })
-
-  it(`Happy path with account consent`, () => {
-    acpLoginPage.login(Credentials.tppUsername, Credentials.defaultPassword);
-    consentSelfServicePage.clickOnApplicationCard()
-    consentSelfServiceApplicationPage.expandAccountsTab()
-    consentSelfServiceApplicationPage.checkAccount()
-    consentSelfServiceApplicationPage.expandAccountConsentRow()
-  })
-})
 
 describe(`Consent self service app`, () => {
   const acpLoginPage: AcpLoginPage = new AcpLoginPage();
@@ -104,7 +64,7 @@ describe(`Consent self service app`, () => {
     acpLoginPage.login(Credentials.tppUsername, Credentials.defaultPassword);
     consentSelfServicePage.clickOnApplicationCard()
     consentSelfServiceApplicationPage.expandAccountsTab()
-    consentSelfServiceApplicationPage.checkAccount()
+    consentSelfServiceApplicationPage.checkAccount("22289")
     consentSelfServiceApplicationPage.expandAccountConsentRow()
   })
 
@@ -112,7 +72,7 @@ describe(`Consent self service app`, () => {
     acpLoginPage.login(Credentials.tppUsername, Credentials.defaultPassword);
     consentSelfServicePage.clickOnApplicationCard()
     consentSelfServiceApplicationPage.expandPaymentsTab()
-    consentSelfServiceApplicationPage.checkAccount()
+    consentSelfServiceApplicationPage.checkAccount("22289")
     consentSelfServiceApplicationPage.expandPaymentConsentRow()
     consentSelfServiceApplicationPage.assertAmount(amount)
   })
