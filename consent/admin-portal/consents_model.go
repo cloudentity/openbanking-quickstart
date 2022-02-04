ec07d3e9f36fb8c34544d0a95e9bbfb9b15e8d29
Billy <bbray@cloudentity.com>
2022-02-04 12:24:57 -0500

    added consent model

--- consent/admin-portal/consents_model.go
+++ consent/admin-portal/consents_model.go
@@ -1,5 +1,32 @@
 package main
 
+import "github.com/go-openapi/strfmt"
+
+type Consent struct {
+	AccountIDs  []string        `json:"AccountIDs"`
+	ConsentID   string          `json:"ConsentID"`
+	ClientID    string          `json:"client_id"`
+	TenantID    string          `json:"tenant_id"`
+	ServerID    string          `json:"server_id"`
+	Status      string          `json:"Status"`
+	Type        string          `json:"type"`
+	CreatedAt   strfmt.DateTime `json:"CreationDateTime"`
+	ExpiresAt   strfmt.DateTime `json:"ExpirationDateTime"`
+	UpdatedAt   strfmt.DateTime `json:"StatusUpdateDateTime"`
+	Permissions []string        `json:"Permissions"`
+
+	DebtorAccountIdentification string `json:"DebtorAccountIdentification"`
+	DebtorAccountName           string `json:"DebtorAccountName"`
+
+	CreditorAccountIdentification string `json:"CreditorAccountIdentification"`
+	CreditorAccountName           string `json:"CreditorAccountName"`
+
+	Currency string `json:"Currency"`
+	Amount   string `json:"Amount"`
+
+	CompletionDateTime strfmt.DateTime `json:"CompletionDateTime"`
+}
+
 type ClientConsents struct {
 	Client
 	Consents []Consent `json:"consents"`
