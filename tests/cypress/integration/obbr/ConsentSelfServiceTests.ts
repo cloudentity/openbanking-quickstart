66146749fdab14f7943725ce7a13492acaa5a47c
Dylan Martin <dmartin@cloudentity.com>
2022-03-07 14:29:33 -0500

    add consent self service obbr tests

---
+++ tests/cypress/integration/obbr/ConsentSelfServiceTests.ts
@@ -0,0 +1,78 @@
+import {AcpLoginPage} from '../../pages/acp/AcpLoginPage';
+import {ConsentPage} from '../../pages/consent/ConsentPage';
+import {ErrorPage} from '../../pages/ErrorPage';
+import {Credentials} from "../../pages/Credentials";
+import {ConsentSelfServicePage} from '../../pages/consent-self-service/ConsentSelfServicePage';
+import {Urls} from "../../pages/Urls";
+import {MfaPage} from "../../pages/mfa/MfaPage";
+import {FinancrooLoginPage} from "../../pages/financroo/FinancrooLoginPage";
+import {FinancrooWelcomePage} from "../../pages/financroo/FinancrooWelcomePage";
+import {FinancrooAccountsPage} from "../../pages/financroo/accounts/FinancrooAccountsPage";
+import {FinancrooInvestmentsPage} from "../../pages/financroo/investments/FinancrooInvestmentsPage";
+import {FinancrooContributePage} from "../../pages/financroo/investments/FinancrooContributePage";
+import {ConsentSelfServiceApplicationPage} from "../../pages/consent-self-service/ConsentSelfServiceApplicationPage";
+import {EnvironmentVariables} from "../../pages/EnvironmentVariables"
+import { FinancrooModalPage } from '../../pages/financroo/accounts/FinancrooModalPage';
+
+
+describe(`Consent self service app`, () => {
+  const acpLoginPage: AcpLoginPage = new AcpLoginPage();
+  const consentPage: ConsentPage = new ConsentPage();
+  const errorPage: ErrorPage = new ErrorPage();
+  const consentSelfServicePage: ConsentSelfServicePage = new ConsentSelfServicePage();
+  const consentSelfServiceApplicationPage: ConsentSelfServiceApplicationPage = new ConsentSelfServiceApplicationPage();
+  const mfaPage: MfaPage = new MfaPage();
+  const financrooLoginPage: FinancrooLoginPage = new FinancrooLoginPage();
+  const financrooWelcomePage: FinancrooWelcomePage = new FinancrooWelcomePage();
+  const financrooModalPage: FinancrooModalPage = new FinancrooModalPage();
+  const financrooAccountsPage: FinancrooAccountsPage = new FinancrooAccountsPage();
+  const financrooInvestmentsPage: FinancrooInvestmentsPage = new FinancrooInvestmentsPage();
+  const financrooContributePage: FinancrooContributePage = new FinancrooContributePage();
+  const environmentVariables: EnvironmentVariables = new EnvironmentVariables();
+
+  before(() => {
+    financrooLoginPage.visit()
+    Urls.clearLocalStorage()
+    financrooLoginPage.visit()
+    financrooLoginPage.login()
+    acpLoginPage.login(Credentials.financrooUsername, Credentials.defaultPassword)
+    financrooWelcomePage.connectSantanderBank()
+    acpLoginPage.login(Credentials.tppUsername, Credentials.defaultPassword)
+    if (environmentVariables.isMfaEnabled()) {
+      mfaPage.typePin()
+    }
+    consentPage.confirm()
+    financrooModalPage.assertThatModalIsDisplayed()
+
+    financrooLoginPage.visit()
+    financrooAccountsPage.assertThatPageIsDisplayed()
+  })
+
+  beforeEach(() => {
+    consentSelfServicePage.visit(true)
+  })
+
+  it(`Happy path with account consent`, () => {
+    acpLoginPage.login(Credentials.tppUsername, Credentials.defaultPassword);
+    consentSelfServicePage.clickOnApplicationCard()
+    consentSelfServiceApplicationPage.expandAccountsTab()
+    consentSelfServiceApplicationPage.checkAccount("94088392") // TODO: 
+    consentSelfServiceApplicationPage.expandAccountConsentRow()
+  })
+
+  it(`Revoke consent`, () => {
+    acpLoginPage.login(Credentials.tppUsername, Credentials.defaultPassword);
+    consentSelfServicePage.clickOnApplicationCard()
+    consentSelfServiceApplicationPage.expandAccountsTab()
+    consentSelfServiceApplicationPage.assertNumberOfConsents(1)
+    consentSelfServiceApplicationPage.expandAccountConsentRow()
+    consentSelfServiceApplicationPage.clickRevokeAccessButton() 
+    consentSelfServiceApplicationPage.assertNumberOfConsents(0)
+  })
+
+  it(`Cancel ACP login`, () => {
+    acpLoginPage.cancel();
+    errorPage.assertError("The user rejected the authentication")
+  })
+
+})
