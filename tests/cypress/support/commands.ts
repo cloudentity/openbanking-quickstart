456e56fd2cd864304fad04ab9fe55de5f3a43581
Rajmund Nowicki <72209717+RajmundNowicki@users.noreply.github.com>
2022-03-11 14:59:54 +0100

    Feature/aut 5552 cypress disable samesite cookie restrictions (#129)
    * AUT-5552 Added disableSameSiteCookieRestrictions command.
    
    * Fix import command
    
    * AUT-5552 Added index.ts with disableSameSiteCookieRestrictions command.
    
    * AUT-5552 Updated Cypress tests with disableSameSiteCookieRestrictions command.
    
    * AUT-5552 Moved disableSameSiteCookieRestrictions command to visit function.
    
    * AUT-5552 Removed disableSameSiteCookieRestrictions command from TppTechnicalTests.ts.
    
    Co-authored-by: Bartosz Wereszczak <bwereszczak@syntegrity.com>

---
+++ tests/cypress/support/commands.ts
@@ -0,0 +1,22 @@
+Cypress.Commands.add("disableSameSiteCookieRestrictions", () => {
+  cy.intercept("*", (req) => {
+    req.on("response", (res) => {
+      if (!res.headers["set-cookie"]) {
+        return;
+      }
+
+      const disableSameSite = (headerContent: string): string => {
+        return headerContent.replace(
+          /samesite=(lax|strict)/gi,
+          "samesite=none"
+        );
+      };
+
+      if (Array.isArray(res.headers["set-cookie"])) {
+        res.headers["set-cookie"] = res.headers["set-cookie"].map(disableSameSite) as any;
+      } else {
+        res.headers["set-cookie"] = disableSameSite(res.headers["set-cookie"]);
+      }
+    });
+  });
+});
