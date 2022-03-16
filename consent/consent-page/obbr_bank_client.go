b1cc29e04246a5b3b5a907a9b7981261223b888a
Dylan Martin <dmartin@cloudentity.com>
2022-03-16 14:10:06 -0400

    fix internal balances for obbr consent page

--- consent/consent-page/obbr_bank_client.go
+++ consent/consent-page/obbr_bank_client.go
@@ -69,36 +69,62 @@ func ToInternalOBBRAccounts(data models.ResponseAccountList) InternalAccounts {
 	return InternalAccounts{Accounts: accounts}
 }
 
+type OBBRBalances struct {
+	Data []struct {
+		models.AccountBalancesData
+		AccountID string `json:"accountId"`
+	} `json:"data"`
+}
+
 // TODO: map response to InternalBalances
 func (c *OBBRBankClient) GetInternalBalances(id string) (BalanceResponse, error) {
 	var (
 		request  *http.Request
 		response *http.Response
 		bytes    []byte
-		resp     = BalanceResponse{}
+		resp     OBBRBalances
 		err      error
 	)
 
 	if request, err = http.NewRequest("GET", fmt.Sprintf("%s/internal/balances?id=%s", c.baseURL, id), nil); err != nil {
-		return resp, err
+		return BalanceResponse{}, err
 	}
 
 	if response, err = c.Client.Do(request); err != nil {
-		return resp, err
+		return BalanceResponse{}, err
 	}
 	defer response.Body.Close()
 
 	if bytes, err = ioutil.ReadAll(response.Body); err != nil {
-		return resp, err
+		return BalanceResponse{}, err
 	}
 
 	if response.StatusCode != 200 {
-		return resp, fmt.Errorf("unexpected status code: %d, body: %s", response.StatusCode, string(bytes))
+		return BalanceResponse{}, fmt.Errorf("unexpected status code: %d, body: %s", response.StatusCode, string(bytes))
 	}
 
 	if err = json.Unmarshal(bytes, &resp); err != nil {
-		return resp, err
+		return BalanceResponse{}, err
 	}
 
-	return resp, nil
+	return c.ToBalanceResponse(resp), nil
+}
+
+func (c *OBBRBankClient) ToBalanceResponse(data OBBRBalances) BalanceResponse {
+	var balances []Balance
+
+	for _, r := range data.Data {
+		balances = append(balances, Balance{
+			AccountID: r.AccountID,
+			Amount: BalanceAmount{
+				Amount:   fmt.Sprintf("%.2f", *r.AvailableAmount),
+				Currency: *r.AvailableAmountCurrency,
+			},
+		})
+	}
+	return BalanceResponse{
+		Data: BalanceData{
+			Balance: balances,
+		},
+	}
 }
