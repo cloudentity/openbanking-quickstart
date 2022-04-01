8a389b601b1d6a0232eba7b800d36e63e4b9a015
Rajmund Nowicki <rnowicki@cloudentity.com>
2022-04-01 16:19:29 +0200

    AUT-5709 Updated clean_saas.sh script with T flag.

--- scripts/clean_saas.sh
+++ scripts/clean_saas.sh
@@ -1,4 +1,4 @@
-docker-compose -f docker-compose.acp.local.yaml exec runner sh -c \
+docker-compose -f docker-compose.acp.local.yaml exec -T runner sh -c \
     "go run ./scripts/go/clean_saas.go \
         -spec=$1 \
         -tenant=$2 \
