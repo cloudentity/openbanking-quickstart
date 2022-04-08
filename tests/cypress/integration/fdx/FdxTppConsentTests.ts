d9944b154586c2acc8b443864de8bcdc854befe7
Rajmund Nowicki <rnowicki@cloudentity.com>
2022-04-08 13:51:54 +0200

    AUT-5791 Updated FdxTppConsentTests.ts file

--- tests/cypress/integration/fdx/FdxTppConsentTests.ts
+++ tests/cypress/integration/fdx/FdxTppConsentTests.ts
@@ -1,115 +1,124 @@
-import {TppAuthenticatedPage} from '../../pages/tpp/TppAuthenticatedPage';
-import {TppIntentPage} from '../../pages/tpp/TppIntentPage';
-import {TppLoginPage} from '../../pages/tpp/TppLoginPage';
-import {AcpLoginPage} from '../../pages/acp/AcpLoginPage';
-import {ConsentPage} from '../../pages/consent/ConsentPage';
+import { AcpLoginPage } from "../../pages/acp/AcpLoginPage";
+import { ConsentPage } from "../../pages/consent/ConsentPage";
+import { Credentials } from "../../pages/Credentials";
+import { Urls } from "../../pages/Urls";
+import { MfaPage } from "../../pages/mfa/MfaPage";
+import { EnvironmentVariables } from "../../pages/EnvironmentVariables";
+import { FdxTppLoginPage } from "../../pages/fdx-tpp/FdxTppLoginPage";
+import { FdxTppIntentRegisteredPage } from "../../pages/fdx-tpp/FdxTppIntentRegisteredPage";
+import { FdxTppAuthenticatedPage } from "../../pages/fdx-tpp/FdxTppAuthenticatedPage";
 import {ErrorPage} from '../../pages/ErrorPage';
-import {Credentials} from "../../pages/Credentials";
-import {Urls} from "../../pages/Urls";
-import {MfaPage} from "../../pages/mfa/MfaPage";
-import {EnvironmentVariables} from "../../pages/EnvironmentVariables"
-import { FdxTppLoginPage } from '../../pages/fdx-tpp/FdxTppLoginPage';
-import { FdxTppIntentRegisteredPage } from '../../pages/fdx-tpp/FdxTppIntentRegisteredPage';
 
-describe(`Tpp technical app`, () => {
+describe(`FDX Tpp consent app`, () => {
   const fdxTppLoginPage: FdxTppLoginPage = new FdxTppLoginPage();
-  const fdxTppIntentRegisteredPage: FdxTppIntentRegisteredPage = new FdxTppIntentRegisteredPage();
-
-  const tppAuthenticatedPage: TppAuthenticatedPage = new TppAuthenticatedPage();
-  const tppIntentPage: TppIntentPage = new TppIntentPage();
-  const tppLoginPage: TppLoginPage = new TppLoginPage();
+  const fdxTppIntentRegisteredPage: FdxTppIntentRegisteredPage =
+    new FdxTppIntentRegisteredPage();
+  const fdxTppAuthenticatedPage: FdxTppAuthenticatedPage =
+    new FdxTppAuthenticatedPage();
   const acpLoginPage: AcpLoginPage = new AcpLoginPage();
   const consentPage: ConsentPage = new ConsentPage();
-  const errorPage: ErrorPage = new ErrorPage();
   const mfaPage: MfaPage = new MfaPage();
   const environmentVariables: EnvironmentVariables = new EnvironmentVariables();
+  const errorPage: ErrorPage = new ErrorPage();
 
-  const basicPermission: string = `ReadAccountsBasic`;
-  const detailPermission: string = `ReadAccountsDetail`;
-
+  const digitalBankingAccountId: string = `96534987`;
+  const savingsAccountId: string = `1000001`;
+  const savings2AccountId: string = `1000002`;
 
   beforeEach(() => {
-    fdxTppLoginPage.visit()
-    Urls.clearLocalStorage()
+    fdxTppLoginPage.visit();
+    Urls.clearLocalStorage();
     fdxTppLoginPage.visit();
   });
 
-  it(`TEST`, () => {
+  [
+    [digitalBankingAccountId, savings2AccountId],
+    [savingsAccountId],
+    [savings2AccountId],
+  ].forEach((accountsIds) => {
+    it(`Happy path with selcted accounts: ${accountsIds}`, () => {
+      fdxTppLoginPage.assertThatPageIsDisplayed();
+      fdxTppLoginPage.assertThatAuthorizationDetailsAreDispalyed();
+      fdxTppLoginPage.clickNext();
+
+      fdxTppIntentRegisteredPage.assertThatPageIsDisplayed();
+      fdxTppIntentRegisteredPage.assertThatRequestUriFieldsAreNotEmpty();
+      fdxTppIntentRegisteredPage.clickLogin();
+
+      acpLoginPage.login(Credentials.tppUsername, Credentials.defaultPassword);
+      if (environmentVariables.isMfaEnabled()) {
+        mfaPage.typePin();
+      }
+
+      consentPage.checkAccounts(accountsIds);
+      consentPage.expandPermissions();
+      consentPage.assertPermissions(4);
+      consentPage.confirm();
+
+      fdxTppAuthenticatedPage.assertThatPageIsDisplayed();
+      fdxTppAuthenticatedPage.assertThatTokenResponseFieldIsNotEmpty();
+      fdxTppAuthenticatedPage.assertThatAccessTokenFieldIsNotEmpty();
+      fdxTppAuthenticatedPage.assertThatConsentResponseFieldContainsAccountsIds(
+        accountsIds
+      );
+
+      fdxTppAuthenticatedPage.clickTryNext();
+      fdxTppLoginPage.assertThatPageIsDisplayed();
+    });
+  });
+
+  it(`Happy path with not selected account`, () => {
     fdxTppLoginPage.assertThatPageIsDisplayed();
-    fdxTppLoginPage.assertThatAuthorizationDetailsAreDispalyed()
+    fdxTppLoginPage.assertThatAuthorizationDetailsAreDispalyed();
     fdxTppLoginPage.clickNext();
 
     fdxTppIntentRegisteredPage.assertThatPageIsDisplayed();
-    fdxTppIntentRegisteredPage.assertThatRequestUriIsDisplayed();
+    fdxTppIntentRegisteredPage.assertThatRequestUriFieldsAreNotEmpty();
     fdxTppIntentRegisteredPage.clickLogin();
 
+    acpLoginPage.login(Credentials.tppUsername, Credentials.defaultPassword);
+    if (environmentVariables.isMfaEnabled()) {
+      mfaPage.typePin();
+    }
+
+    consentPage.confirm();
+
+    fdxTppAuthenticatedPage.assertThatPageIsDisplayed();
+    fdxTppAuthenticatedPage.assertThatConsentResponseFieldNotContainsAccountsIds(
+      [digitalBankingAccountId, savingsAccountId, savings2AccountId]
+    );
+  });
+
+  it('Cancel on consent page', () => {
+    fdxTppLoginPage.assertThatPageIsDisplayed();
+    fdxTppLoginPage.assertThatAuthorizationDetailsAreDispalyed();
+    fdxTppLoginPage.clickNext();
+
+    fdxTppIntentRegisteredPage.assertThatPageIsDisplayed();
+    fdxTppIntentRegisteredPage.assertThatRequestUriFieldsAreNotEmpty();
+    fdxTppIntentRegisteredPage.clickLogin();
+
+    acpLoginPage.login(Credentials.tppUsername, Credentials.defaultPassword);
+    if (environmentVariables.isMfaEnabled()) {
+      mfaPage.typePin();
+    }
+
+    consentPage.checkAccounts([digitalBankingAccountId]);
+    consentPage.cancel();
+
+    errorPage.assertError(`acp returned an error: rejected: `);
   })
 
+  it('Cancel on ACP login', () => {
+    fdxTppLoginPage.assertThatPageIsDisplayed();
+    fdxTppLoginPage.assertThatAuthorizationDetailsAreDispalyed();
+    fdxTppLoginPage.clickNext();
 
+    fdxTppIntentRegisteredPage.assertThatPageIsDisplayed();
+    fdxTppIntentRegisteredPage.assertThatRequestUriFieldsAreNotEmpty();
+    fdxTppIntentRegisteredPage.clickLogin();
 
-
-
-
-
-
-
-
-
-
-
-
-    // [
-    //   [basicPermission, detailPermission],
-    //   [basicPermission],
-    //   [detailPermission]
-      
-    // ].forEach(permissions => {
-    //   it(`Happy path with permissions: ${permissions}`, () => {
-    //     tppLoginPage.checkBasicPermission(permissions.includes(basicPermission))
-    //     tppLoginPage.checkDetailPermission(permissions.includes(detailPermission))
-    //     tppLoginPage.next();
-    //     if (!permissions.includes(basicPermission) && !permissions.includes(detailPermission)) {
-    //       errorPage.assertError(`Invalid consent request`)
-    //     } else {
-    //       tppIntentPage.login();
-    //       acpLoginPage.login(Credentials.tppUsername, Credentials.defaultPassword);
-    //       if (environmentVariables.isMfaEnabled()) {
-    //         mfaPage.typePin()
-    //       }
-    //       consentPage.expandPermissions()
-    //       consentPage.assertPermissions(permissions.length)
-    //       consentPage.confirm();
-    //       if (!permissions.includes(basicPermission) && permissions.includes(detailPermission)) {
-    //         errorPage.assertError(`failed to call bank get accounts`)
-    //       } else {
-    //         tppAuthenticatedPage.assertSuccess()
-    //       }
-    //     }
-    //   })
-    // });
-
- 
-
-  it(`Cancel on ACP login`, () => {
-    // FIXME restore when this fix has been made
-    // https://github.com/cloudentity/openbanking-quickstart/pull/108
-    // tppLoginPage.next();
-    // tppIntentPage.login();
-    // acpLoginPage.cancel();
-    // errorPage.assertError(`The user rejected the authentication`)
+    acpLoginPage.cancel();
+    errorPage.assertError(`The user rejected the authentication`);
   })
-
-  it(`Cancel on consent`, () => {
-    // FIXME restore when this fix has been made
-    // https://github.com/cloudentity/openbanking-quickstart/pull/108
-    // tppLoginPage.next();
-    // tppIntentPage.login();
-    // acpLoginPage.login(Credentials.tppUsername, Credentials.defaultPassword);
-    // if (environmentVariables.isMfaEnabled()) {
-    //   mfaPage.typePin()
-    // }
-    // consentPage.cancel()
-    // errorPage.assertError(`rejected`)
-  })
-
-})
+});
