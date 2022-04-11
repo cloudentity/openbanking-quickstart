7f13a52d88e286491df2a7865505ebfea39cc210
Rajmund Nowicki <rnowicki@cloudentity.com>
2022-04-11 13:57:33 +0200

    AUT-5822 Fixed typos

--- tests/cypress/pages/fdx-tpp/FdxTppLoginPage.ts
+++ tests/cypress/pages/fdx-tpp/FdxTppLoginPage.ts
@@ -6,7 +6,7 @@ export class FdxTppLoginPage {
   private readonly authorizationDetailsContentLocator: string = `#fdx-login-authorization-details`;
   private readonly nextButtonLocator: string = `#fdx-login-next-button`;
 
-  private expectedAythorizationDetails: string =
+  private expectedAuthorizationDetails: string =
     '{"authorization_details":[{"type":"fdx_v1.0","consentRequest":' +
     '{"durationType":"ONE_TIME","lookbackPeriod":60,"resources":' +
     '[{"resourceType":"ACCOUNT","dataClusters":["ACCOUNT_DETAILED","TRANSACTIONS","STATEMENTS"]},' +
@@ -24,10 +24,10 @@ export class FdxTppLoginPage {
     cy.get(this.nextButtonLocator, { timeout: 3000 }).should("be.visible");
   }
 
-  public assertThatAuthorizationDetailsAreDispalyed(): void {
+  public assertThatAuthorizationDetailsAreDisplayed(): void {
     cy.get(this.authorizationDetailsContentLocator).then((element) => {
       let text: string = element.text().replace(/\s/g, "");
-      expect(text).to.contain(this.expectedAythorizationDetails);
+      expect(text).to.contain(this.expectedAuthorizationDetails);
     });
   }
 
