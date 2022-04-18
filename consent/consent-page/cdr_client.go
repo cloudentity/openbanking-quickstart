dad0778b6f7ae24f3fcac58ae787a3f04c165ed5
Dylan Martin <dmartin@cloudentity.com>
2022-04-18 12:19:19 -0400

    replace mock cdr get internal accounts with http call

--- consent/consent-page/cdr_client.go
+++ consent/consent-page/cdr_client.go
@@ -4,9 +4,15 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/json"
+	"io"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/clientcredentials"
@@ -14,6 +20,7 @@ import (
 
 type CDRBankClient struct {
 	httpClient *http.Client
+	bankURL    string
 	cc         clientcredentials.Config
 }
 
@@ -57,21 +64,48 @@ func NewCDRBankClient(config Config) *CDRBankClient {
 			TokenURL:     config.BankClientConfig.TokenURL,
 			Scopes:       config.BankClientConfig.Scopes,
 		},
+		bankURL: config.BankClientConfig.URL.String(),
 	}
 }
 
-// TODO: expose endpoint on mock data holder instead of redundant hardcoded mocking in every application
-// TODO: this id will be customer id and will be passed in request body
 func (c *CDRBankClient) GetInternalAccounts(id string) (InternalAccounts, error) {
-	// test token retrieval
-	token, err := c.cc.Token(context.WithValue(context.Background(), oauth2.HTTPClient, c.httpClient))
-	if err != nil {
-		logrus.Infof("error retrieving cc token %v", err)
-	} else {
-		logrus.Infof("retrieved cc token with no errors %+v", token)
+	var (
+		token    *oauth2.Token
+		request  *http.Request
+		response *http.Response
+		err      error
+	)
+
+	if token, err = c.cc.Token(context.WithValue(context.Background(), oauth2.HTTPClient, c.httpClient)); err != nil {
+		return InternalAccounts{}, errors.Wrapf(err, "failed to get client credentials token for internal bank api call")
 	}
 
-	if id == "user" {
+	if request, err = http.NewRequest(http.MethodPost, c.bankURL+"/internal/accounts", strings.NewReader(
+		url.Values{
+			"customer_id": []string{id},
+		}.Encode(),
+	)); err != nil {
+		return InternalAccounts{}, err
+	}
+
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	request.Header.Set("Authorization", "Bearer "+token.AccessToken)
+
+	if response, err = c.httpClient.Do(request); err != nil {
+		return InternalAccounts{}, errors.Wrapf(err, "internal bank accounts api call failed")
+	}
+
+	if response.StatusCode >= http.StatusBadRequest {
+		var raw []byte
+		if raw, err = io.ReadAll(response.Body); err != nil {
+			return InternalAccounts{}, errors.Wrap(err, "internal bank accounts api call failed")
+		}
+		return InternalAccounts{}, errors.Wrap(errors.New(string(raw)), "internal bank accounts api call failed")
+	}
+
+	return c.accountsResponseToInternalAccounts(response)
+
+	/*if id == "user" {
 		return InternalAccounts{
 			Accounts: []InternalAccount{
 				{
@@ -136,7 +170,61 @@ func (c *CDRBankClient) GetInternalAccounts(id string) (InternalAccounts, error)
 				},
 			},
 		},
-	}, nil
+	}, nil*/
+}
+
+/*
+[
+{
+"account_id": "1000001",
+"display_name": "************001",
+"account_alias": "Savings Account"
+},
+{
+"account_id": "1000002",
+"display_name": "************002",
+"account_alias": "An old Savings Account"
+},
+{
+"account_id": "96534987",
+"display_name": "************003",
+"account_alias": "Basic Mortgage"
+}
+]
+*/
+type CDRInternalAccountsResponse struct {
+	Accounts []CDRInternalAccount
+}
+
+type CDRInternalAccount struct {
+	AccountID    string `json:"account_id"`
+	Displayname  string `json:"display_name"`
+	AccountAlias string `json:"account_alias"`
+}
+
+func (c *CDRBankClient) accountsResponseToInternalAccounts(response *http.Response) (accounts InternalAccounts, err error) {
+	var (
+		responseBytes               []byte
+		cdrInternalAccountsResponse CDRInternalAccountsResponse
+	)
+	defer response.Body.Close()
+
+	if responseBytes, err = ioutil.ReadAll(response.Body); err != nil {
+		return accounts, err
+	}
+
+	if err = json.Unmarshal(responseBytes, &cdrInternalAccountsResponse); err != nil {
+		return accounts, err
+	}
+
+	for _, acc := range cdrInternalAccountsResponse.Accounts {
+		accounts.Accounts = append(accounts.Accounts, InternalAccount{
+			ID:   acc.AccountID,
+			Name: acc.AccountAlias,
+		})
+	}
+
+	return accounts, nil
 }
 
 // TODO: mock data holder cdr app doesn't even have this data yet
