aab9e1343f1bff2c028b439a9c8779df1f304620
Dylan Martin <dmartin@cloudentity.com>
2022-04-28 16:35:14 -0400

    cdr accounts endpoint introspection and filtering

--- apps/bank/cdr_get_accounts_handler.go
+++ apps/bank/cdr_get_accounts_handler.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"strings"
+
 	cdr "github.com/cloudentity/acp-client-go/clients/openbanking/client/c_d_r"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
 type CDRGetAccountsHandler struct {
@@ -15,12 +18,11 @@ func NewCDRGetAccountsHandler(server *Server) GetEndpointLogic {
 }
 
 func (h *CDRGetAccountsHandler) SetIntrospectionResponse(c *gin.Context) *Error {
-	/*var err error
+	var err error
 	if h.introspectionResponse, err = h.CDRIntrospectAccountsToken(c); err != nil {
 		logrus.Errorf("introspect cdr token for accounts failed with %+v", err)
 		return ErrBadRequest.WithMessage("failed to introspect token")
 	}
-	logrus.Infof("introspect succeeded")*/
 	return nil
 }
 
@@ -34,6 +36,10 @@ func (h *CDRGetAccountsHandler) BuildResponse(c *gin.Context, data BankUserData)
 }
 
 func (h *CDRGetAccountsHandler) Validate(c *gin.Context) *Error {
+	scopes := strings.Split(h.introspectionResponse.Scope, " ")
+	if !has(scopes, "bank:accounts.basic:read") {
+		return ErrForbidden.WithMessage("token has no bank:accounts.basic:read scope granted")
+	}
 	return nil
 }
 
@@ -42,5 +48,11 @@ func (h *CDRGetAccountsHandler) GetUserIdentifier(c *gin.Context) string {
 }
 
 func (h *CDRGetAccountsHandler) Filter(c *gin.Context, data BankUserData) BankUserData {
-	return data
+	var ret BankUserData
+	for _, account := range data.CDRAccounts {
+		if has(h.introspectionResponse.AccountIDs, *account.AccountID) {
+			ret.CDRAccounts = append(ret.CDRAccounts, account)
+		}
+	}
+	return ret
 }
