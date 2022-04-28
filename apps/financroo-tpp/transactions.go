1ea5cf29a796ffdfae0798d2c38b144f49284c6b
Dylan Martin <dmartin@cloudentity.com>
2022-04-28 08:15:21 -0400

    cdr get transactions wip

--- apps/financroo-tpp/transactions.go
+++ apps/financroo-tpp/transactions.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"github.com/cloudentity/openbanking-quickstart/openbanking/cdr/banking/client/banking"
 	"github.com/cloudentity/openbanking-quickstart/openbanking/obuk/accountinformation/client/transactions"
 	"github.com/cloudentity/openbanking-quickstart/openbanking/obuk/accountinformation/models"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
 type Transaction struct {
@@ -37,6 +39,37 @@ func (o *OBBRClient) GetTransactions(c *gin.Context, accessToken string, bank Co
 	return []Transaction{}, nil
 }
 
-func (o *CDRClient) GetTransactions(c *gin.Context, accessToken string, bank ConnectedBank) ([]Transaction, error) {
-	return []Transaction{}, nil
+func (o *CDRClient) GetTransactions(c *gin.Context, accessToken string, bank ConnectedBank) (transactionsData []Transaction, err error) {
+	var (
+		resp *banking.GetTransactionsOK
+	)
+
+	// will need to loop
+	if resp, err = o.Banking.Banking.GetTransactions(
+		banking.NewGetTransactionsParams().
+			WithDefaults().
+			WithAccountID("1000001"),
+	); err != nil {
+		return transactionsData, err
+	}
+
+	for _, a := range resp.Payload.Data.Transactions {
+		transactionsData = append(transactionsData, Transaction{
+			OBTransaction6: models.OBTransaction6{
+				AccountID:       (*models.AccountID)(a.AccountID),
+				TransactionID:   models.TransactionID(a.TransactionID),
+				BookingDateTime: &models.BookingDateTime{},
+				Amount: &models.OBActiveOrHistoricCurrencyAndAmount9{
+					Amount: (*models.OBActiveCurrencyAndAmountSimpleType)(a.Amount),
+				},
+				BankTransactionCode:    &models.OBBankTransactionCodeStructure1{},
+				TransactionInformation: models.TransactionInformation(""),
+			},
+			BankID: bank.BankID,
+		})
+	}
+
+	logrus.Infof("transactions data %v", transactionsData)
+
+	return transactionsData, nil
 }
