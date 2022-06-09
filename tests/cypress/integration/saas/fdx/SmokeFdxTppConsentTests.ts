05574262c2ab87aec87a567ef305c007b99dd24b
Rajmund Nowicki <rnowicki@cloudentity.com>
2022-06-09 15:32:12 +0200

    AUT-6292 uncommented all local stages

--- tests/cypress/integration/saas/fdx/SmokeFdxTppConsentTests.ts
+++ tests/cypress/integration/saas/fdx/SmokeFdxTppConsentTests.ts
@@ -61,7 +61,7 @@ describe(`FDX Tpp consent app`, () => {
 
       fdxTppAuthenticatedPage.clickTryNext();
       fdxTppLoginPage.assertThatPageIsDisplayed();
-      cy.wait(3000)
+      cy.wait(3000) // Workaround due to pipeline issues >>> AUT-6292
     });
   });
 
