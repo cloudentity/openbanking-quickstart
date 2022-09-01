11b546f0fe25b2f8d305b0d7e2f21d7e3363f6fd
ksroda <ksroda@cloudentity.com>
2022-09-01 10:41:30 +0200

    fix

--- consent/self-service-portal/web/app/src/components/applicationDetails/ApplicationDetailsController.tsx
+++ consent/self-service-portal/web/app/src/components/applicationDetails/ApplicationDetailsController.tsx
@@ -55,7 +55,6 @@ function ApplicationDetailsController({
         setError && setError(err.error);
       })
       .finally(() => setProgress(false));
-      
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
