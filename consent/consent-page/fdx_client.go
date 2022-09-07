dd279e4e4fd345f366944d91c89af51068f8bcb4
Billy <bbray@cloudentity.com>
2022-09-07 18:14:26 -0400

    removed hardcode consent handler

--- consent/consent-page/fdx_client.go
+++ consent/consent-page/fdx_client.go
@@ -41,75 +41,36 @@ type DepositAccount struct {
 }
 
 func (c *FDXClient) GetInternalAccounts(ctx context.Context, id string) (InternalAccounts, error) {
-	if id == "user" {
-		var (
-			request     *http.Request
-			response    *http.Response
-			bytes       []byte
-			fdxAccounts FdxAccounts
-			err         error
-		)
+	var (
+		request     *http.Request
+		response    *http.Response
+		bytes       []byte
+		fdxAccounts FdxAccounts
+		err         error
+	)
 
-		if request, err = http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("%s/internal/accounts", c.baseURL), http.NoBody); err != nil {
-			return InternalAccounts{}, err
-		}
-
-		if response, err = c.Client.Do(request); err != nil {
-			return InternalAccounts{}, err
-		}
-		defer response.Body.Close()
-
-		if bytes, err = ioutil.ReadAll(response.Body); err != nil {
-			return InternalAccounts{}, err
-		}
-
-		if response.StatusCode != 200 {
-			return InternalAccounts{}, fmt.Errorf("unexpected status code: %d, body: %s", response.StatusCode, string(bytes))
-		}
-
-		if err = json.Unmarshal(bytes, &fdxAccounts); err != nil {
-			return InternalAccounts{}, err
-		}
-
-		return ToInternalFDXAccounts(fdxAccounts), nil
+	if request, err = http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("%s/internal/accounts", c.baseURL), http.NoBody); err != nil {
+		return InternalAccounts{}, err
 	}
-	return InternalAccounts{
-		Accounts: []InternalAccount{
-			{
-				ID:   "1000001",
-				Name: "Savings",
-				Balance: Balance{
-					AccountID: "96565987",
-					Amount: BalanceAmount{
-						Amount:   "13300.35",
-						Currency: "USD",
-					},
-				},
-			},
-			{
-				ID:   "1000002",
-				Name: "Checking",
-				Balance: Balance{
-					AccountID: "96565987",
-					Amount: BalanceAmount{
-						Amount:   "332.22",
-						Currency: "USD",
-					},
-				},
-			},
-			{
-				ID:   "96534987",
-				Name: "Savings",
-				Balance: Balance{
-					AccountID: "1122334455",
-					Amount: BalanceAmount{
-						Amount:   "1200.00",
-						Currency: "USD",
-					},
-				},
-			},
-		},
-	}, nil
+
+	if response, err = c.Client.Do(request); err != nil {
+		return InternalAccounts{}, err
+	}
+	defer response.Body.Close()
+
+	if bytes, err = ioutil.ReadAll(response.Body); err != nil {
+		return InternalAccounts{}, err
+	}
+
+	if response.StatusCode != 200 {
+		return InternalAccounts{}, fmt.Errorf("unexpected status code: %d, body: %s", response.StatusCode, string(bytes))
+	}
+
+	if err = json.Unmarshal(bytes, &fdxAccounts); err != nil {
+		return InternalAccounts{}, err
+	}
+
+	return ToInternalFDXAccounts(fdxAccounts), nil
 }
 
 func (c *FDXClient) GetInternalBalances(ctx context.Context, id string) (BalanceResponse, error) {
