808abec8f9aaa56581cf53f27263837481ad9df3
Stanisław Jankowski <sjankowski@cloudentity.com>
2022-10-24 15:35:48 +0200

    loop

--- scripts/set_saas_configuration.sh
+++ scripts/set_saas_configuration.sh
@@ -2,24 +2,17 @@
 
 set -e
 
-if [[ ! -v SAAS_TENANT_ID ]]; then
-    echo "SAAS_TENANT_ID is not set"
-    exit 1
-else
-    ./scripts/override_env.sh TENANT ${SAAS_TENANT_ID}
-fi
+#!/bin/bash
 
-if [[ ! -v SAAS_CLIENT_ID ]]; then
-    echo "SAAS_CLIENT_ID is not set"
-    exit 1
-else
-    ./scripts/override_env.sh CONFIGURATION_CLIENT_ID ${SAAS_CLIENT_ID}
-fi
+vars=( SAAS_TENANT_ID SAAS_CLIENT_ID SAAS_CLIENT_SECRET )
+vars2=( TENANT CONFIGURATION_CLIENT_ID CONFIGURATION_CLIENT_SECRET )
 
-if [[ ! -v SAAS_CLIENT_SECRET ]]; then
-    echo "SAAS_CLIENT_SECRET is not set"
-    exit 1
-else
-    ./scripts/override_env.sh CONFIGURATION_CLIENT_SECRET ${SAAS_CLIENT_SECRET}
-fi
 
+for index in ${!vars[*]}; do 
+    if [[ ! -v ${vars[$index]} ]]; then
+        echo "${vars[$index]} is not set"
+        exit 1
+    else
+    ./scripts/override_env.sh ${vars2[$index]} $(printenv ${vars[$index]})
+    fi
+done
