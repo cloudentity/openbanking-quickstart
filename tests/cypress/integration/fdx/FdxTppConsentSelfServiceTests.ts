770063d2f1fa2575b4eb77fb09d5142d33a67f70
Dylan Martin <dmartin@cloudentity.com>
2022-11-08 10:21:16 -0500

    restore tests

--- tests/cypress/integration/fdx/FdxTppConsentSelfServiceTests.ts
+++ tests/cypress/integration/fdx/FdxTppConsentSelfServiceTests.ts
@@ -20,71 +20,70 @@ describe(`FDX TPP Consent admin portal tests`, () => {
 
   const accountsIDs = [Accounts.ids.FDX.checkingAcc, Accounts.ids.FDX.savings1];
 
-  //  Ignored due to BUG - AUT-7531
 
-  // before(() => {
-  //   fdxTppLandingPage.visit();
+  before(() => {
+    fdxTppLandingPage.visit();
 
-  //   fdxTppLandingPage.assertThatPageIsDisplayed();
-  //   fdxTppLandingPage.clickNext();
+    fdxTppLandingPage.assertThatPageIsDisplayed();
+    fdxTppLandingPage.clickNext();
 
-  //   fdxTppIntentRegisteredPage.assertThatPageIsDisplayed();
-  //   fdxTppIntentRegisteredPage.clickLogin();
+    fdxTppIntentRegisteredPage.assertThatPageIsDisplayed();
+    fdxTppIntentRegisteredPage.clickLogin();
 
-  //   acpLoginPage.assertThatModalIsDisplayed("FDX");
-  //   acpLoginPage.loginWithMfaOption();
+    acpLoginPage.assertThatModalIsDisplayed("FDX");
+    acpLoginPage.loginWithMfaOption();
 
-  //   accountConsentPage.assertPermissions(4);
-  //   accountConsentPage.assertThatAccountsAreNotVisible([
-  //     Accounts.ids.FDX.checkingAcc,
-  //     Accounts.ids.FDX.savings1,
-  //     Accounts.ids.FDX.savings2,
-  //   ]);
+    accountConsentPage.assertPermissions(4);
+    accountConsentPage.assertThatAccountsAreNotVisible([
+      Accounts.ids.FDX.checkingAcc,
+      Accounts.ids.FDX.savings1,
+      Accounts.ids.FDX.savings2,
+    ]);
 
-  //   accountConsentPage.clickContinue();
-  //   accountConsentPage.checkAccounts(accountsIDs);
-  //   accountConsentPage.clickAgree();
+    accountConsentPage.clickContinue();
+    accountConsentPage.checkAccounts(accountsIDs);
+    accountConsentPage.clickAgree();
 
-  //   fdxTppAuthenticatedPage.assertThatPageIsDisplayed();
-  //   fdxTppAuthenticatedPage.assertThatTokenResponseFieldIsNotEmpty();
-  //   fdxTppAuthenticatedPage.assertThatAccessTokenFieldIsNotEmpty();
-  //   fdxTppAuthenticatedPage.assertThatConsentResponseFieldContainsAccountsIds(accountsIDs);
+    fdxTppAuthenticatedPage.assertThatPageIsDisplayed();
+    fdxTppAuthenticatedPage.assertThatTokenResponseFieldIsNotEmpty();
+    fdxTppAuthenticatedPage.assertThatAccessTokenFieldIsNotEmpty();
+    fdxTppAuthenticatedPage.assertThatConsentResponseFieldContainsAccountsIds(accountsIDs);
 
-  //   fdxTppAuthenticatedPage.clickTryNext();
-  //   fdxTppLandingPage.assertThatPageIsDisplayed();
-  // });
+    fdxTppAuthenticatedPage.clickTryNext();
+    fdxTppLandingPage.assertThatPageIsDisplayed();
+  });
 
-  // beforeEach(() => {
-  //   consentSelfServicePage.visit(true);
+  beforeEach(() => {
+    consentSelfServicePage.visit(true);
 
-  //   acpLoginPage.assertThatModalIsDisplayed("Bank customers");
-  //   acpLoginPage.login();
+    acpLoginPage.assertThatModalIsDisplayed("Bank customers");
+    acpLoginPage.login();
 
-  //   consentSelfServicePage.clickOnApplicationCardWithName("Developer TPP");
-  // });
+    consentSelfServicePage.clickOnApplicationCardWithName("Developer TPP");
+  });
 
   it(`Happy path with account consent`, () => {
-    // consentSelfServiceApplicationPage.expandAccountsTab();
-    // consentSelfServiceApplicationPage.checkAccountHasStatus(accountsIDs[0], "Authorised");
-    // consentSelfServiceApplicationPage.checkAccountHasStatus(accountsIDs[1], "Authorised");
-    // consentSelfServiceApplicationPage.expandAccountConsentRow();
+    consentSelfServiceApplicationPage.expandAccountsTab();
+    consentSelfServiceApplicationPage.checkAccountHasStatus(accountsIDs[0], "Authorised");
+    consentSelfServiceApplicationPage.checkAccountHasStatus(accountsIDs[1], "Authorised");
+    consentSelfServiceApplicationPage.expandAccountConsentRow();
 
-    // consentSelfServiceAccountDetailsPage.assertThatAccountDetailsAreVisible()
-    // consentSelfServiceAccountDetailsPage.assertAccount(accountsIDs[0]);
-    // consentSelfServiceAccountDetailsPage.assertAccount(accountsIDs[1]);
+    consentSelfServiceAccountDetailsPage.assertThatAccountDetailsAreVisible()
+    consentSelfServiceAccountDetailsPage.assertAccount(accountsIDs[0]);
+    consentSelfServiceAccountDetailsPage.assertAccount(accountsIDs[1]);
   });
 
   it(`Revoke account consent`, () => {
-    // consentSelfServiceApplicationPage.expandAccountsTab();
-    // consentSelfServiceApplicationPage.assertAuthorisedAccountRowExists(accountsIDs[0]);
-    // consentSelfServiceApplicationPage.assertAuthorisedAccountRowExists(accountsIDs[1]);
-    // consentSelfServiceApplicationPage.expandAccountConsentRow();
+    consentSelfServiceApplicationPage.expandAccountsTab();
+    consentSelfServiceApplicationPage.assertAuthorisedAccountRowExists(accountsIDs[0]);
+    consentSelfServiceApplicationPage.assertAuthorisedAccountRowExists(accountsIDs[1]);
+    consentSelfServiceApplicationPage.expandAccountConsentRow();
 
-    // consentSelfServiceAccountDetailsPage.assertThatAccountDetailsAreVisible();
-    // consentSelfServiceAccountDetailsPage.clickRevokeAccessButton();
-    // consentSelfServiceAccountDetailsPage.assertThatRevokeAccountDetailsAreVisible();
-    // consentSelfServiceAccountDetailsPage.confirmRevokeAccessAction();
+    consentSelfServiceAccountDetailsPage.assertThatAccountDetailsAreVisible();
+    consentSelfServiceAccountDetailsPage.clickRevokeAccessButton();
+    consentSelfServiceAccountDetailsPage.assertThatRevokeAccountDetailsAreVisible();
+    consentSelfServiceAccountDetailsPage.confirmRevokeAccessAction();
 
-    // consentSelfServicePage.assertThatNoAccountsPageIsDisplayed();
+    consentSelfServicePage.assertThatNoAccountsPageIsDisplayed();
   });
 });
