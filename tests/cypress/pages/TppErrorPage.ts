6e0cde6ad7450e57479817d10359b25b0c95d807
Rajmund Nowicki <rnowicki@cloudentity.com>
2022-11-14 14:49:21 +0100

    AUT-7576 Updated assertions in TppErrorPage.ts

--- tests/cypress/pages/TppErrorPage.ts
+++ tests/cypress/pages/TppErrorPage.ts
@@ -1,16 +1,20 @@
 export class TppErrorPage {
-  private readonly errorTitleLocator: string = `#fdx-consent-error`;
-  private readonly errorDescriptionLocator: string = `#fdx-consent-error-description`;
-  private readonly errorCauseLocator: string = `#fdx-consent-error-cause`;
-  private readonly traceIdLocator: string = `#fdx-consent-trace-id`;
-  private readonly tryNextButtonLocator: string = `#fdx-consent-error-try-again-button`;
+  private readonly errorTitleLocator: string = `#tpp-consent-error`;
+  private readonly errorDescriptionLocator: string = `#tpp-consent-error-description`;
+  private readonly errorCauseLocator: string = `#tpp-consent-error-cause`;
+  private readonly traceIdLocator: string = `#tpp-consent-trace-id`;
+  private readonly tryNextButtonLocator: string = `#tpp-consent-error-try-again-button`;
 
 
-  public assertThatErrorPageIsDisplayed(error: string, description: string, cause: string): void {
+  public assertThatRejectConsentErrorPageIsDisplayed(error: string, description: string, cause: string): void {
+    this.assertThatCancelLoginErrorPageIsDisplayed(error, description);
+    cy.get(this.errorCauseLocator).contains("cause: " + cause);
+  }
+
+  public assertThatCancelLoginErrorPageIsDisplayed(error: string, description: string): void {
     cy.get(this.tryNextButtonLocator, { timeout: 3000 }).should("be.visible");
     cy.get(this.errorTitleLocator).contains(error);
     cy.get(this.errorDescriptionLocator).contains("description: " + description);
-    cy.get(this.errorCauseLocator).contains("cause: " + cause);
     cy.get(this.traceIdLocator).then((element) => {
       let text: string = element.text();
       expect(text).to.match(/ID\: [a-zA-Z0-9]+/);
