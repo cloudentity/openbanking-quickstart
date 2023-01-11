766494b2b9702cdee0c067f37ee95b9374abd02e
Ireneusz Kawalec <ikawalec@cloudentity.com>
2023-01-11 16:03:00 +0100

    uncomment CDR tests (#299)

--- tests/cypress/integration/saas/cdr/SmokeFinancrooConsentAdminTests.ts
+++ tests/cypress/integration/saas/cdr/SmokeFinancrooConsentAdminTests.ts
@@ -7,89 +7,88 @@ import { FinancrooAccountsPage } from "../../../pages/financroo/accounts/Financr
 import { Accounts } from "../../../pages/Accounts";
 import { ConsentAdminPage } from "../../../pages/consent-admin/ConsentAdminPage";
 
-// FIXME: 
-// describe(`Smoke Financroo Consent admin portal tests`, () => {
-//   const financrooLoginPage: FinancrooLoginPage = new FinancrooLoginPage();
-//   const financrooWelcomePage: FinancrooWelcomePage = new FinancrooWelcomePage();
-//   const acpLoginPage: AcpLoginPage = new AcpLoginPage();
-//   const accountConsentPage: AccountConsentPage = new AccountConsentPage();
-//   const financrooModalPage: FinancrooModalPage = new FinancrooModalPage();
-//   const financrooAccountsPage: FinancrooAccountsPage = new FinancrooAccountsPage();
-//   const consentAdminPage: ConsentAdminPage = new ConsentAdminPage();
+describe(`Smoke Financroo Consent admin portal tests`, () => {
+  const financrooLoginPage: FinancrooLoginPage = new FinancrooLoginPage();
+  const financrooWelcomePage: FinancrooWelcomePage = new FinancrooWelcomePage();
+  const acpLoginPage: AcpLoginPage = new AcpLoginPage();
+  const accountConsentPage: AccountConsentPage = new AccountConsentPage();
+  const financrooModalPage: FinancrooModalPage = new FinancrooModalPage();
+  const financrooAccountsPage: FinancrooAccountsPage = new FinancrooAccountsPage();
+  const consentAdminPage: ConsentAdminPage = new ConsentAdminPage();
 
 
-//   beforeEach(() => {
-//     financrooLoginPage.visit();
-//     financrooLoginPage.login();
+  beforeEach(() => {
+    financrooLoginPage.visit();
+    financrooLoginPage.login();
 
-//     financrooWelcomePage.reconnectGoBank();
+    financrooWelcomePage.reconnectGoBank();
 
-//     acpLoginPage.assertThatModalIsDisplayed();
-//     acpLoginPage.loginWithMfaOption();
-//   });
+    acpLoginPage.assertThatModalIsDisplayed();
+    acpLoginPage.loginWithMfaOption();
+  });
 
-//   it(`Happy path with revoking consent from Consent management page`, () => {
-//     const accountsIDs = [Accounts.ids.CDR.loan, Accounts.ids.CDR.checking];
+  it(`Happy path with revoking consent from Consent management page`, () => {
+    const accountsIDs = [Accounts.ids.CDR.loan, Accounts.ids.CDR.checking];
 
-//     accountConsentPage.checkAccounts(accountsIDs);
-//     accountConsentPage.expandPermissions();
-//     accountConsentPage.assertPermissionsDetails(
-//       "Purpose for sharing data",
-//       "To uncover insights that can improve your financial well being."
-//     );
-//     accountConsentPage.clickAgree();
+    accountConsentPage.checkAccounts(accountsIDs);
+    accountConsentPage.expandPermissions();
+    accountConsentPage.assertPermissionsDetails(
+      "Purpose for sharing data",
+      "To uncover insights that can improve your financial well being."
+    );
+    accountConsentPage.clickAgree();
 
-//     financrooModalPage.assertThatModalIsDisplayed();
-//     financrooModalPage.close();
+    financrooModalPage.assertThatModalIsDisplayed();
+    financrooModalPage.close();
 
-//     financrooAccountsPage.assertThatPageIsDisplayed();
-//     financrooAccountsPage.assertAccountsSyncedNumber(accountsIDs.length);
-//     financrooAccountsPage.assertAccountsIds(accountsIDs);
+    financrooAccountsPage.assertThatPageIsDisplayed();
+    financrooAccountsPage.assertAccountsSyncedNumber(accountsIDs.length);
+    financrooAccountsPage.assertAccountsIds(accountsIDs);
 
-//     consentAdminPage.visit(true);
-//     consentAdminPage.login();
+    consentAdminPage.visit(true);
+    consentAdminPage.login();
 
-//     consentAdminPage.assertThatConsentManagementTabIsDisplayed();
-//     consentAdminPage.searchAccount(Accounts.ids.CDR.loan);
-//     consentAdminPage.assertAccountResult(Accounts.ids.CDR.loan);
-//     consentAdminPage.assertClientAccountWithStatus("Financroo", "Active");
-//     consentAdminPage.manageAccount("Financroo");
-//     consentAdminPage.assertConsentsDetails();
-//     consentAdminPage.revokeClientConsentByAccountName("Financroo");
-//     consentAdminPage.assertClientAccountWithStatus("Financroo", "Inactive");
-//   });
+    consentAdminPage.assertThatConsentManagementTabIsDisplayed();
+    consentAdminPage.searchAccount(Accounts.ids.CDR.loan);
+    consentAdminPage.assertAccountResult(Accounts.ids.CDR.loan);
+    consentAdminPage.assertClientAccountWithStatus("Financroo", "Active");
+    consentAdminPage.manageAccount("Financroo");
+    consentAdminPage.assertConsentsDetails();
+    consentAdminPage.revokeClientConsentByAccountName("Financroo");
+    consentAdminPage.assertClientAccountWithStatus("Financroo", "Inactive");
+  });
 
-//   it(`Happy path with revoking consent from Third party providers page`, () => {
-//     const accountsIDs = [Accounts.ids.CDR.savings];
+  it(`Happy path with revoking consent from Third party providers page`, () => {
+    const accountsIDs = [Accounts.ids.CDR.savings];
 
-//     accountConsentPage.checkAccounts(accountsIDs);
-//     accountConsentPage.expandPermissions();
-//     accountConsentPage.assertPermissionsDetails(
-//       "Purpose for sharing data",
-//       "To uncover insights that can improve your financial well being."
-//     );
-//     accountConsentPage.clickAgree();
+    accountConsentPage.checkAccounts(accountsIDs);
+    accountConsentPage.expandPermissions();
+    accountConsentPage.assertPermissionsDetails(
+      "Purpose for sharing data",
+      "To uncover insights that can improve your financial well being."
+    );
+    accountConsentPage.clickAgree();
 
-//     financrooModalPage.assertThatModalIsDisplayed();
-//     financrooModalPage.close();
+    financrooModalPage.assertThatModalIsDisplayed();
+    financrooModalPage.close();
 
-//     financrooAccountsPage.assertThatPageIsDisplayed();
-//     financrooAccountsPage.assertAccountsSyncedNumber(accountsIDs.length);
-//     financrooAccountsPage.assertAccountsIds(accountsIDs);
+    financrooAccountsPage.assertThatPageIsDisplayed();
+    financrooAccountsPage.assertAccountsSyncedNumber(accountsIDs.length);
+    financrooAccountsPage.assertAccountsIds(accountsIDs);
 
-//     consentAdminPage.visit(true);
-//     consentAdminPage.login();
+    consentAdminPage.visit(true);
+    consentAdminPage.login();
 
-//     consentAdminPage.assertThatConsentManagementTabIsDisplayed();
-//     consentAdminPage.revokeClientConsent();
-//   });
+    consentAdminPage.assertThatConsentManagementTabIsDisplayed();
+    consentAdminPage.revokeClientConsent();
+  });
 
 
-//   afterEach(() => {
-//     financrooLoginPage.visit();
-//     financrooLoginPage.login();
+  afterEach(() => {
+    financrooLoginPage.visit();
+    financrooLoginPage.login();
 
-//     financrooAccountsPage.assertThatAccountsAreDisconnected();
-//   });
+    financrooAccountsPage.assertThatAccountsAreDisconnected();
+  });
 
-// });
+});
