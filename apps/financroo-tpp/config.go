2be74223a09fbb5d2afd12c04e3f60fabc4d2c25
Stanisław Jankowski <110402706+stanislaw-jankowski@users.noreply.github.com>
2023-03-16 09:05:18 +0100

    AUT-8031: Use PS key for signing client assertion and ES key to sign request object (#322)
    * Use PS key for signing client assertion
    
    * Use ES key to sign request object
    
    * bump go mod

--- apps/financroo-tpp/config.go
+++ apps/financroo-tpp/config.go
@@ -20,27 +20,31 @@ const (
 )
 
 type Config struct {
-	Port            int          `env:"PORT" envDefault:"8091"`
-	DBFile          string       `env:"DB_FILE" envDefault:"/app/data/my.db"`
-	ACPURL          string       `env:"ACP_URL" validate:"required,url"`
-	ACPInternalURL  string       `env:"ACP_MTLS_URL" validate:"required,url"`
-	AppHost         string       `env:"APP_HOST" validate:"required"`
-	Tenant          string       `env:"TENANT" validate:"required"`
-	UIURL           string       `env:"UI_URL" validate:"required,url"`
-	CertFile        string       `env:"CERT_FILE" envDefault:"/certs/tpp_cert.pem"`
-	KeyFile         string       `env:"KEY_FILE" envDefault:"/certs/tpp_key.pem"`
-	CookieHashKey   string       `env:"COOKIE_HASH_KEY" envDefault:"secret-key"`
-	CookieBlockKey  string       `env:"COOKIE_BLOCK_KEY" envDefault:"this-is-32-len-block-key"`
-	FeatureFlags    FeatureFlags `env:"FEATURE_FLAGS"`
-	Spec            Spec         `env:"SPEC" validate:"required"`
-	BankURL         string       `env:"BANK_URL" validate:"required"`
-	RootCA          string       `env:"ROOT_CA" envDefault:"/certs/ca.pem"`
-	ClientID        string       `env:"CLIENT_ID" envDefault:"bugkgm23g9kregtu051g"`
-	ClientSecret    string       `env:"CLIENT_SECRET" envDefault:"-TlfoycUiE0qNi-XUBFDfTxMlhHTCjVxOF6pLrWZbQA"` // only required for fdx
-	ServerID        string       `env:"OPENBANKING_SERVER_ID" validate:"required"`
-	EnableTLSServer bool         `env:"ENABLE_TLS_SERVER" envDefault:"true"`
-	Currency        string       `env:"CURRENCY"` // optional custom currency, one of=USD AUD GBP BRL EUR
-	ClientScopes    []string
+	Port                        int          `env:"PORT" envDefault:"8091"`
+	DBFile                      string       `env:"DB_FILE" envDefault:"/app/data/my.db"`
+	ACPURL                      string       `env:"ACP_URL" validate:"required,url"`
+	ACPInternalURL              string       `env:"ACP_MTLS_URL" validate:"required,url"`
+	AppHost                     string       `env:"APP_HOST" validate:"required"`
+	Tenant                      string       `env:"TENANT" validate:"required"`
+	UIURL                       string       `env:"UI_URL" validate:"required,url"`
+	CertFile                    string       `env:"CERT_FILE" envDefault:"/certs/tpp_cert.pem"`
+	KeyFile                     string       `env:"KEY_FILE" envDefault:"/certs/tpp_key.pem"`
+	CookieHashKey               string       `env:"COOKIE_HASH_KEY" envDefault:"secret-key"`
+	CookieBlockKey              string       `env:"COOKIE_BLOCK_KEY" envDefault:"this-is-32-len-block-key"`
+	FeatureFlags                FeatureFlags `env:"FEATURE_FLAGS"`
+	Spec                        Spec         `env:"SPEC" validate:"required"`
+	BankURL                     string       `env:"BANK_URL" validate:"required"`
+	RootCA                      string       `env:"ROOT_CA" envDefault:"/certs/ca.pem"`
+	ClientID                    string       `env:"CLIENT_ID" envDefault:"bugkgm23g9kregtu051g"`
+	ClientSecret                string       `env:"CLIENT_SECRET" envDefault:"-TlfoycUiE0qNi-XUBFDfTxMlhHTCjVxOF6pLrWZbQA"` // only required for fdx
+	ServerID                    string       `env:"OPENBANKING_SERVER_ID" validate:"required"`
+	EnableTLSServer             bool         `env:"ENABLE_TLS_SERVER" envDefault:"true"`
+	Currency                    string       `env:"CURRENCY"` // optional custom currency, one of=USD AUD GBP BRL EUR
+	AssertionSigningAlg         string       `env:"ASSERTION_SIGNING_ALG" envDefault:"PS256"`
+	AssertionSigningKeyFile     string       `env:"ASSERTION_SIGNING_KEY_FILE" envDefault:"/certs/private.ps.pem"`
+	RequestObjectSigningAlg     string       `env:"REQUEST_OBJECT_SIGNING_ALG" envDefault:"ES256"`
+	RequestObjectSigningKeyFile string       `env:"REQUEST_OBJECT_SIGNING_KEY_FILE" envDefault:"/certs/private.es.pem"`
+	ClientScopes                []string
 }
 
 func (c *Config) SetImplicitValues() {
